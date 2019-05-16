c4adb92a868f07f154c81c9a2fd778b05d4a720f
suwei <1473040317@qq.com>
2019-05-16 17:20:59 +0800

    function: Stage方法新增

--- src/main/java/fx/test/StageTest.java
+++ src/main/java/fx/test/StageTest.java
@@ -24,18 +24,32 @@ public class StageTest extends Application {
         primaryStage.setMaxHeight(700);
         primaryStage.setMinWidth(400);
         primaryStage.setMinHeight(300);
+        primaryStage.setOpacity(0.5); //设置透明度
+        primaryStage.setAlwaysOnTop(true); //窗口置顶
+        primaryStage.setX(100);  //窗口左上角位置
+        primaryStage.setY(100);  //窗口右上角位置
         primaryStage.heightProperty().addListener(new ChangeListener<Number>() {  //监听窗口高度变化
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("旧值: "+oldValue+",新值: " + newValue);
                 System.out.println("当前高度是:" + primaryStage.getHeight());
             }
         });
         primaryStage.widthProperty().addListener(new ChangeListener<Number>() {  //监听窗口宽度变化
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                System.out.println("旧值: "+oldValue+",新值: " + newValue);
                 System.out.println("当前宽度是:" + primaryStage.getWidth());
+            }
+        });
+        primaryStage.xProperty().addListener(new ChangeListener<Number>() {  //监听窗口位置变化
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                System.out.println("当前x是:" + primaryStage.getX());
+            }
+        });
+        primaryStage.yProperty().addListener(new ChangeListener<Number>() {  //监听窗口位置变化
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                System.out.println("当前y是:" + primaryStage.getY());
             }
         });
         primaryStage.show();  //显示窗口
